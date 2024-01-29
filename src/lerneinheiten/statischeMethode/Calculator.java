5302305a8bd4463e22933448f397c57765b9b909
sgglko <jonas.siggelkow@unitybox.de>
2024-01-29 08:34:07 +0100

    ✨ added Calculater, Add and Subtract Methods

---
+++ src/lerneinheiten/statischeMethode/Calculator.java
@@ -0,0 +1,29 @@
+package lerneinheiten.statischeMethode;
+
+/**
+ * Taschenrechner
+ *
+ * @author Jonas Siggelkow
+ * @version 1.0
+ */
+public class Calculator {
+    /**
+     * Addiert zwei Zahlen und gibt das Ergebnis zurück
+     * @param x 1. Zahl
+     * @param y 2. Zahl
+     * @return int
+     */
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    /**
+     * Subtrahiert zwei Zahlen und gibt das Ergebnis zurück
+     * @param x 1. Zahl
+     * @param y 2. Zahl
+     * @return int
+     */
+    public static int subtract(int x, int y) {
+        return x - y;
+    }
+}
---
+++ src/lerneinheiten/statischeMethode/StaticMethods.java
@@ -0,0 +1,9 @@
+package lerneinheiten.statischeMethode;
+
+public class StaticMethods {
+    public static void main(String[] args) {
+        int result = Calculator.add(4,3);
+        System.out.println(result);
+        System.out.println(Calculator.subtract(5,8));
+    }
+}
