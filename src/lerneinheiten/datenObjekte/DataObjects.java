8dad73b63c8f708fbd3ea4b3f13fb6b65b118e34
sgglko <jonas.siggelkow@unitybox.de>
2024-01-29 10:56:42 +0100

    ✨ added Class to Show how to declare variable data objects

---
+++ src/lerneinheiten/datenObjekte/DataObjects.java
@@ -0,0 +1,30 @@
+package lerneinheiten.datenObjekte;
+
+/**
+ * Variable Datenobjekte
+ *
+ * @author Jonas Siggelkow
+ * @version 1.0
+ */
+public class DataObjects {
+    public static void main(String[] args) {
+
+        //Deklaration variabler Datenobjekte
+
+        int i;
+        String name, firstName, lastName, fullName, firstNameAndLastName;
+        double weightKG, amountEuro, sizeM;
+        byte age;
+        boolean isHappy;
+
+        // Initialisierung
+        i = 5;
+        name = "Hans Peter";
+        isHappy = true;
+        sizeM = 1.79;
+
+        char gender = 'M';
+
+        System.out.println(i);
+    }
+}
